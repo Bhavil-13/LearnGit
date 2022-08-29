cacbfd98c36d3947962a832325410dbc1f8b64c3
Bhavil-13 <98340959+Bhavil-13@users.noreply.github.com>
2022-08-29 22:51:05 +0530

    Update main.py

--- MailBot/main.py
+++ MailBot/main.py
@@ -3,5 +3,5 @@ from abc import msg
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('bhavilsharma001@gmail.com', '12P@0PP1330b')
-server.sendmail('bhavilsharma001@gmail.com', 'bhavilsharma83@gmail.com', msg.message)
+server.login('sender@gmail.com','password')
+server.sendmail('sender@gmail.com', 'rcvr@gmail.com', msg.message)
